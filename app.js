a3fbd296d368aade97b7e83931fdde0007dbe0ae
AcAntellAno <acantellano1996@gmail.com>
2018-09-07 10:39:39 -0700

    Added source urls

--- app.js
+++ app.js
@@ -16,3 +16,7 @@ routes(app);
 var server = app.listen(port, () => {
     console.log("The api is running on " + port);
 });
+
+// https://www.codementor.io/wapjude/creating-a-simple-rest-api-with-expressjs-in-5min-bbtmk51mq
+// https://medium.freecodecamp.org/building-a-simple-node-js-api-in-under-30-minutes-a07ea9e390d2
+// https://medium.freecodecamp.org/build-restful-api-with-authentication-under-5-minutes-using-loopback-by-expressjs-no-programming-31231b8472ca
