f55c4c60be3ed23b21962a5e5f2bb283d6afec59
AcAntellAno <acantellano1996@gmail.com>
2018-09-07 12:15:31 -0700

    Added user endpoint

--- routes/routes.js
+++ routes/routes.js
@@ -1,6 +1,37 @@
+var faker = require('faker');
 var appRouter = (app) => {
     app.get('/', (req, res) => {
         res.status(200).send('You hit the RESTful API');
+    });
+
+    app.get('/user', (req, res) => {
+        var data = ({
+            firstName : faker.name.firstName(),
+            lastName : faker.name.lastName(),
+            username: faker.internet.userName(),
+            email: faker.internet.email()
+        });
+        res.status(200).send(data);
+    });
+
+    app.get('/user/:num', (req, res) => {
+        var users = [];
+        var num = req.params.num;
+
+        if(isFinite(num) && num > 0){
+            for(i = 0; i <= num - 1; i++){
+                users.push({
+                    firstName : faker.name.firstName(),
+                    lastName : faker.name.lastName(),
+                    username: faker.internet.userName(),
+                    email: faker.internet.email()
+                });
+            }
+            res.status(200).send(users);
+
+        } else {
+            res.status(400).send({message: 'Invalid Number Supplied...'});
+        }
     });
 }
 
